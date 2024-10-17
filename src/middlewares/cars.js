660316791b6f1024833bc0394c56923033906152
ryanlikestocode44 <nicholasryan770@gmail.com>
2024-10-17 21:44:42 +0700

    Added middleware for cars.json

---
+++ src/middlewares/cars.js
@@ -0,0 +1,22 @@
+const { zod } = require('zod');
+const { BadRequestError } = require('../utils');
+
+// Validasi untuk mengambil semua data cars
+export const validateGetAllCars = (req, res, next) => {
+};
+
+// Validasi untuk mengambil data car dari id
+export const validateGetCarById = (req, res, next) => {
+};
+
+// Validasi untuk menambah data car baru
+export const validateAddCar = (req, res, next) => {
+};
+
+// Validasi untuk merubah data car
+export const validateUpdateCar = (req, res, next) => {
+};
+
+// Validasi untuk menghapus data car
+export const validateDeleteCarById = (req, res, next) => {
+};
