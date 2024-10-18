4ea202428960aa57e7ad055230d6c3b630a0e3e0
Ryan Nicholas Purba <123624589+ryanlikestocode44@users.noreply.github.com>
2024-10-18 21:29:56 +0700

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,15 @@
 const express = require("express");
 const modelsRouter = require("./models")
+const manufacturesRouter = require("./manufactures")
+const transmissionsRouter = require("./transmissions")
+const typesRouter = require("./types")
 
 const router = express.Router();
 
 router.use("/models", modelsRouter);
+router.use("/manufactures", manufacturesRouter);
+router.use("/transmissions", transmissionsRouter);
+router.use("/types", typesRouter);
 
 router.get('/', (req, res) => {
   res.send({
